f5bf6ab291b864c40bef170699edc378239e457f
linym <rexlin2002001@gmail.com>
2014-03-19 00:13:54 +0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
 $db_host = "dbhome.cs.nctu.edu.tw";
-$db_name = "yshsueh_cs";
-$db_user = "yshsueh_cs";
-$db_password = "26662666";
+$db_name = "linym_cs";
+$db_user = "linym_cs";
+$db_password = "xup6u.4aup6";
 $dsn = "mysql:host=$db_host;dbname=$db_name";
 $db = new PDO($dsn, $db_user, $db_password);
 ?>
